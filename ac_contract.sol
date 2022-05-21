9423a7df11c9a3fa8c65888bfd8a698098a91e0e
0guztumay <90459481+0guztumay@users.noreply.github.com>
2022-05-21 20:01:22 +0300

    Add files via upload

--- ac_contract.sol
+++ ac_contract.sol
@@ -9,16 +9,16 @@ contract oguzac{
     address[]wallet;
 
     function setAcOwner(uint256 _ac,uint256 tokenvalue , uint256 _degree) public returns(uint256){
-        require(ac[_ac]>0&&ac[_ac]<5,"There is only 4 A.C. here.");
         require(paidToken[_ac]<tokenvalue,"Please give higher price");
-        paidToken[_ac]=tokenvalue;
+        require(ac[_ac]>0&&ac[_ac]<5,"There is only 4 A.C. here.");
         acDegree[_ac]=_degree;
+        paidToken[_ac]=tokenvalue;
         wallet[_ac]=msg.sender;
         return paidToken[_ac];
     }
     function setDegree(uint256 _ac, uint256 _degree)public{
-        require(ac[_ac]>0&&ac[_ac]<5,"There is only 4 A.C. here.");
         require(wallet[_ac]==msg.sender,"The owner of the air conditioner does not appear here");
+        require(ac[_ac]>0&&ac[_ac]<5,"There is only 4 A.C. here.");
         require(_degree>15&&_degree<31,"Please enter a value between 16-30");
         acDegree[_ac]=_degree;
     }
